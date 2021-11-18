0c8a618fbce13503c7c96551fad5efc812aba064
Corby <chichols@rcp.com>
2021-11-17 21:45:58 -0600

    Add project files.

---
+++ Delegates/CallBackEvents.cs
@@ -0,0 +1,22 @@
+﻿
+
+#region using statements
+
+using DataJuggler.Net6.Enumerations;
+using System.Collections.Generic;
+
+#endregion
+
+namespace DataJuggler.Net6.Delegates
+{
+
+    #region ItemChangedCallback(object itemChanged, ChangeTypeEnum changeType);
+    /// <summary>
+    /// This delegate is used to recieve notification event if an item is added, removed or changed.
+    /// </summary>
+    /// <param name="itemChanged"></param>
+    /// <param name="changeType"></param>
+    public delegate void ItemChangedCallback(object itemChanged, ChangeTypeEnum changeType);
+    #endregion
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# DataJuggler.Net
+DataJuggler.Net is a core component of several Data Juggler projects including DataTier.Net, DB Compare and XML Mirror.
+
+Another project that uses CSharpClassWriter is called DataJuggler.Excelerate
+https://github.com/DataJuggler/Excelerate
+
+
